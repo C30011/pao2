333cbfea640c55bb72dcdcee59f16f271f432f14
C30011 <cleberfraga5354@gmail.com>
2026-04-13 07:37:42 -0300

    Criar pão.py

---
+++ pão.py
@@ -0,0 +1,43 @@
+print("--- Sistema de Reservas: Hotel Porto Seguro ---")
+
+nome = input("Nome do responsável: ")
+if nome == "":
+     print("O Nome é Inválido!")
+     exit()
+
+dia_in = int(input("Dia do check-in: "))
+mes_in = int(input("Mês do check-in: "))
+ano_in = int(input("Ano do check-in: "))
+
+dia_out = int(input("\nDia do check-out: "))
+mes_out = int(input("Mês do check-out: "))
+ano_out = int(input("Ano do check-out: "))
+
+if (ano_out < ano_in) or \
+(ano_out == ano_in and mes_out < mes_in) or \
+(ano_out == ano_in and mes_out == mes_in and dia_out <= dia_in):
+     print("A data de check-out deve ser posterior ao check-in.")
+     exit()
+
+total_dias = (dia_out - dia_in) + (mes_out - mes_in) * 30 + (ano_out - ano_in) * 365
+custo_total = total_dias * diaria
+
+quarto = input("Tipo de quarto (Pataxó, Ancestral, Descobrimento): ")
+
+diaria = 0
+if quarto == "Pataxó":
+     diaria = 100
+elif quarto == "Ancestral":
+     diaria = 180
+elif quarto == "Descobrimento":
+     diaria = 250
+else:
+     print("Quarto inválido")
+     exit()
+
+print("\n" + "="*30)
+print(f"Nome do Responsável: {nome}")
+print(f"Data de Checkin e de Checkout: {dia_in}/{mes_in}/{ano_in} a {dia_out}/{mes_out}/{ano_out}")
+print(f"Tipo de Quarto: {quarto}")
+print(f"Custo da Reserva: R$ {custo_total}")
+print("="*30)
